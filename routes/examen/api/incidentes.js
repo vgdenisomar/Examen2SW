231afd42c34f3996e9f7c9f993f466e40952aa74
denis <vgdenisomar@gmail.com>
2019-07-09 19:14:20 -0600

    los dos put y el delete

--- routes/examen/api/incidentes.js
+++ routes/examen/api/incidentes.js
@@ -10,7 +10,7 @@ function routerInit(db) {
         "tipo": '',
         "estado": '',
         "usuarioRegistra": '',
-        "usuarioRegistra": '',
+        "usuarioAsignado": '',
         "fechaHoraAsignado": '',
         "fechaHoraCerrado": ''
     };
@@ -40,6 +40,9 @@ function routerInit(db) {
         var newElement = Object.assign({},
             incidentesStruct,
             req.body,
+            {
+                "estado": "registrado",
+            }
         );
 
         incidentesCollection.insertOne(newElement, {}, (err, result) => {
@@ -51,6 +54,44 @@ function routerInit(db) {
             return res.status(200).json({ "n": result.insertedCount, "obj": result.ops[0] });
         });
     });
+
+    router.put('/asign/:id', (req, res, next) => {
+        var query = {"_id": new ObjectID(req.params.id)};
+        var update = { "$set":{"estado": "asignado", "fechaHoraAsignado":new Date().getTime(), "usuarioAsignado":req.body.usuarioAsignado}};
+        incidentesCollection.updateOne(query, update, (err, rst) => {
+          if(err){
+            console.log(err);
+            return res.status(400).json({"error": "Error al actualizar documento"});
+          }
+          return res.status(200).json(rst);
+        }); 
+      });
+
+      router.put('/close/:id', (req, res, next) => {
+        var query = {"_id": new ObjectID(req.params.id)};
+        var update = { "$set":{"estado": "cerrado", "fechaHoraCerrado":new Date().getTime(), "usuarioRegistra":req.body.usuarioRegistra}};
+        incidentesCollection.updateOne(query, update, (err, rst) => {
+          if(err){
+            console.log(err);
+            return res.status(400).json({"error": "Error al actualizar documento"});
+          }
+          return res.status(200).json(rst);
+        }); 
+      });
+
+            
+      router.delete('/:id', (req, res, next) => {
+        var query = { "_id": new ObjectID(req.params.id) };
+        incidentesCollection.removeOne(query, (err, rslt) => {
+          if (err) {
+            console.log(err);
+            return res.status(404).json({ "error": "No se pudo eliminar el incidente" });
+          }
+    
+          return res.status(200).json(rslt);
+        })
+    }); // delete
+
 
     return router
 
